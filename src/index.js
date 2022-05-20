212e0ff585520125c6421199b3b681f653416dd7
Rafael <rafaelongo45@gmail.com>
2022-05-20 16:24:09 -0300

    First commit. Initial setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDom from 'react-dom';
+
+import App from './Components/App/App';
+
+import './Assets/reset.css';
+import './Assets/style.css';
+
+ReactDom.render(<App/>, document.querySelector('.root'));
