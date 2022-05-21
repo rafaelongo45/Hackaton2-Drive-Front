e2413206e0bd097e1c469d099ad14ec139812db2
Rafael <rafaelongo45@gmail.com>
2022-05-20 22:52:35 -0300

    Created question page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,9 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 
 import Home from ".././Home/Home";
+import Question from '../Question/Question';
 import RenderSignUp from ".././SignUp/SignUp";
 import RenderSignIn from ".././SignIn/SignIn";
-import Question from '../../Question/Question';
+import PostAnswer from '../Question/RenderPostAnswer';
 import RenderSubjectsQuestion from ".././RenderSubjectsQuestion/RenderSubjectsQuestion";
 
 import UserContext from "../Contexts/UserContexts";
@@ -18,10 +19,11 @@ function App(){
       <UserContext.Provider value ={ {userData, setUserData}}>
         <Routes>
           <Route path = '/' element={<Home />}/>
-          <Route path = '/signup' element={<RenderSignUp />}/>
-          <Route path = '/signin' element={<RenderSignIn />}/>
-          <Route path = '/subjects-question' element={<RenderSubjectsQuestion />}/>
-          <Route path = '/question' element = {<Question />} />
+          <Route path = '/signup' element={<RenderSignUp />} />
+          <Route path = '/signin' element={<RenderSignIn />} />
+          <Route path = '/subjects-question' element={<RenderSubjectsQuestion />} />
+          <Route path = '/question' element ={<Question />} />
+          <Route path = '/answer' element = {<PostAnswer />} />
         </Routes>
       </UserContext.Provider>
     </BrowserRouter>
